be837d00b2560e2f04ce5278c83cbe009e5dd6fd
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-06-18 19:50:24 +0530

    connect flask to mysql

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
 app=Flask(__name__)
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='root'
+app.config['MYSQL_DB']='study_assistant'
+db=MySQL(app)
 @app.route('/')
 def home():
     return "flask is running"    
