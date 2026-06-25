38a227b4186236935d46c4d3f18fab5f97ba54b7
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-06-25 23:25:45 +0530

    update signup handled edge cases but with some bugs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
@@ -14,10 +14,25 @@ def home():
 @app.route('/signup',methods=['POST','GET'])
 def signup():
         if request.method=='POST':
-            cursor=db.connection.cursor()
-            cursor.execute("insert into user (name, password) values (%s, %s)",
-            (request.form['name'], request.form['password']))
-            db.connection.commit()
+            username=request.form.get('name').strip()
+            password=request.form.get('password')
+        if not username or password:
+            flash("enter paassword and username")
+            return redirect({url_for('signup')})   
+        cursor=db.connection.cursor()
+        existing_user=cursor.execute("select id from user where(name,)")
+        db.connection.close()
+        if existing_user:
+            flash("username already exist")
+            return redirect({url_for('signup')})
+        paassword_hash=generate_password_hashing(paassword)     
+        cursor=db.connection.cursor()
+        cursor.execute("insert into user (username, password_hash, ) " ,values(%s,%s, )
+        (username, password_hash))
+        db.connection.commit()
+        db.connection.close()
+        return render_template("login.html")
     
-        return render_template("signup.html")    
+
+              
 app.run(debug=True)    
