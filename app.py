9f8f3829621a1ad14238c2459d619d7e8b4c8089
JOTHIKA <jothikakumaravadivel@gmail.com>
2026-06-26 19:45:59 +0530

    fix bugs

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+
 
 app=Flask(__name__)
+app.secret_key='joo'
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']='root'
 app.config['MYSQL_DB']='study_assistant'
 db=MySQL(app)
+bycrpt=Bcrypt(app)
 @app.route('/')
 def home():
     return "flask is running"    
@@ -16,22 +20,23 @@ def signup():
         if request.method=='POST':
             username=request.form.get('name').strip()
             password=request.form.get('password')
-        if not username or password:
-            flash("enter paassword and username")
-            return redirect({url_for('signup')})   
-        cursor=db.connection.cursor()
-        existing_user=cursor.execute("select id from user where(name,)")
-        db.connection.close()
-        if existing_user:
-            flash("username already exist")
-            return redirect({url_for('signup')})
-        paassword_hash=generate_password_hashing(paassword)     
-        cursor=db.connection.cursor()
-        cursor.execute("insert into user (username, password_hash, ) " ,values(%s,%s, )
-        (username, password_hash))
-        db.connection.commit()
-        db.connection.close()
-        return render_template("login.html")
+            if not username or not password:
+                flash("enter paassword and username")
+                return redirect(url_for('signup'))   
+            cursor=db.connection.cursor()
+            cursor.execute("select id from user where name=%s", (username, ))
+            existing_user=cursor.fetchone()
+        
+            if existing_user:
+                flash("username already exist")
+                return redirect(url_for('signup'))
+            password_hash=bcrypt.generate_password_hash(password)     
+            cursor=db.connection.cursor()
+            cursor.execute("insert into user (name, password, ) values(%s,%s, )",
+            (username, password_hash))
+            db.connection.commit()
+            return render_template("login.html")
+        return render_template("signup.html")    
     
 
               
